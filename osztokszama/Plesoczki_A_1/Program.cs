ec7c9ff48c8a3ece1427703755645c1f0d7d1949
Ádám Plesoczki <43034243+plesoczkiadam@users.noreply.github.com>
2022-11-27 18:44:02 +0100

    Add files via upload

---
+++ osztokszama/Plesoczki_A_1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plesoczki_A_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int szam, db;
+            Console.WriteLine("Melyik szám osztóinak számat adjam meg?");
+            Console.Write("szám : ");
+            szam = Convert.ToInt32(Console.ReadLine());
+            db = 0;
+            for (int i = 1; i <= szam; i++)
+                if (szam % i == 0)
+                    db++;
+            Console.WriteLine("Az {0} szám osztoinak száma : {1}", szam, db);
+            Console.ReadLine();
+
+        }
+    }
+}
